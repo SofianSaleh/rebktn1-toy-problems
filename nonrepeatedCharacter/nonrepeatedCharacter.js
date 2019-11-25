e3771feeb0f6059b4365c542fa17ba99b9b81be9
SofianSaleh <sofiane.saleh11@gmail.com>
2019-11-25 09:08:28 +0100

    2

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,19 +6,57 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
-var firstNonRepeatedCharacter = function(string) {
+
+ // declare a variable counter and set it to zero.
+ // loop through the array twice and compare
+ // return when the count has the value of 1
+
+
+
+var firstNonRepeatedCharacter = function( string ) {
   // TODO: your solution here
   var count = 0
   	
-  for (var i = 0; i < string.length; i++) {
-  	for (var j = 0; j < 	string.length; j++) {
+  for ( var i = 0; i < string.length; i++ ) {
+  	for ( var j = 0; j < 	string.length; j++ ) {
+
   			if( string[j] === string[i] ) {
   				count++
+
   			}
   	}
-  	if( count === 0 ) {
-  		return str[i]
+  	if( count === 1 ) {
+  		return string[i]
   	}
+  	count = 0
   }
 };
+
+
+
+
+
+var firstNonRepeatedCharacter = function( string ) {
+  // TODO: your solution here
+  var count = 0
+  var i = 0
+  var arr = string.split( '' )
+
+  for (var i = 0; i < string.length; i++ ) {
+
+  	arr.forEach( str => {
+  			if( string[i] === str ) {
+  				count++
+
+  			}
+  	})
+  	if( count === 1 ) {
+
+  		return string[i]
+  	}
+
+  	count = 0
+  }
+};
+
 firstNonRepeatedCharacter('ABA')
