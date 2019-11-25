62108fb88c40fe9135279cb562c06b78725ecf75
SofianSaleh <sofiane.saleh11@gmail.com>
2019-11-25 08:57:28 +0100

    1

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,17 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  var count = 0
+  	
+  for (var i = 0; i < string.length; i++) {
+  	for (var j = 0; j < 	string.length; j++) {
+  			if( string[j] === string[i] ) {
+  				count++
+  			}
+  	}
+  	if( count === 0 ) {
+  		return str[i]
+  	}
+  }
 };
+firstNonRepeatedCharacter('ABA')
