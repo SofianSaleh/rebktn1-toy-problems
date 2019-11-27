f9025258365005a38ebd2b7b697c06f8c8d00502
SofianSaleh <sofiane.saleh11@gmail.com>
2019-11-27 09:42:11 +0100

    done

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -2,7 +2,7 @@
  * Write a function `f(a, b)` which takes two strings as arguments and returns a
  * string containing the characters found in both strings (without duplication), in the
  * order that they appeared in `a`. Remember to skip spaces and characters you
- * have already encountered!
+ * have already enoccurrenceered!
  *
  * Example: commonCharacters('acexivou', 'aegihobu')
  * Returns: 'aeiou'
@@ -12,6 +12,43 @@
 
 
 
+/**
+ * Write a function `f(a, b)` which takes two strings as arguments and returns a
+ * string containing the characters found in both strings (without duplication), in the
+ * order that they appeared in `a`. Remember to skip spaces and characters you
+ * have already enoccurrenceered!
+ *
+ * Example: commonCharacters('acexivou', 'aegihobu')
+ * Returns: 'aeiou'
+ *
+ * Extra credit: Extend your function to handle more than two input strings.
+ */
+
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+  var common = {};
+  var occurrence = 0;
+  var commonLetters = '';
+  var arrayOfCommons = [];
+// Looping through to find the common letters
+  for (var i = 0; i < string1.length; i++) {
+      for (var j = 0; j < string2.length; j++) {
+          if (string1[i] === string2[j]) {
+              occurrence++;
+          }
+      }
+      // Assigining the common letters in an object to avoid conflict
+      common[string1[i]] = occurrence;
+      occurrence = 0;
+  }
+  // Looping through the object and adding the keys with the value of more then ZERO to an array
+  for (var key in common) {
+      if (common[key] > 0) {
+          arrayOfCommons.push(key);
+      }
+  }
+  
+ return arrayOfCommons.join('')
 };
+
+commonCharacters('aaaaa', 'aaaaabu');
