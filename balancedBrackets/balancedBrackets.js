47445ada489e56f49e432ae3a97e54bcdab894e9
Sofian-Saleh <56444632+SofianSaleh@users.noreply.github.com>
2019-12-24 10:47:26 +0200

    3

--- balancedBrackets/balancedBrackets.js
+++ balancedBrackets/balancedBrackets.js
@@ -10,51 +10,38 @@ isBalanced("[{]}") // false
 */
 
 var isBalanced = function(str) {
-    var stack = []
-    var size = 0;
-    var starters = {
-        '(': '(',
-        '[': '[',
-        '{': '{'
-    }
+var stack = []
+var size = 0;
+var starters = {
+'(': ')',
+'[': ']',
+'{': '}'
+}
 
-    var enders = {
-        ')': ')',
-        ']': ']',
-        '}': '}'
-    }
-
-        for(var i = 0; i < str.length; i ++) {
-            if(starters[str[i]]) {
-                stack.push(str[i])
-                size++
-            }else if(enders[str[i]]) {
-                console.log(str[i])
-                if(stack.length === 0) {
-                    return false;
-                }
-                if(str[i] === ')'){
-                    var char = '('
-                }
-                if(str[i] === ']'){
-                    var char = '['
-                }
-                if(str[i] === '}'){
-                    var char = '{'
-                }
-
-
-
-                if(stack[size - 1] === starters[char]){
-                    stack.pop()
-                    size --
-                }else{
-                    return false
-                }
-            }
+for(var i = 0; i < str.length; i ++) {
+    if(starters[str[i]]) {
+        stack.push(str[i])
+        size++
+        console.log(stack)
+    }else if(Object.values(starters).indexOf(str[i]) !== -1) {
+        console.log(str[i])
+        if(stack.length === 0) {
+            return false;
         }
-    return size === 0;
-  
+        var char = Object.keys(starters)[Object.values(starters).indexOf(str[i])]
+
+        console.log(char)
+
+        if(stack[size - 1] === char){
+            stack.pop()
+            size --
+        }else{
+            return false
+        }
+    }
+}
+return size === 0;
+
 };
 isBalanced("(x + y) - (4)") // true
 isBalanced("(((10 ) ()) ((?)(:)))") // true
