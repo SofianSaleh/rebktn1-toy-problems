0f91a211d74d0c7d732beb12524915d28d91b4a9
Sofian-Saleh <56444632+SofianSaleh@users.noreply.github.com>
2019-12-28 09:12:39 +0100

    Done!

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -39,4 +39,4 @@ var evenOccurrence = (array) => {
 evenOccurrence([ 1, 3, 3, 3, 2, 4, 4, 2, 5 ]) // 2
 evenOccurrence([ "cat", "dog", "dig", "cat" ]) // "cat"
 evenOccurrence([[1,2,3], [1,2], [1,2,3]]) // [1,2,3]
-evenOccurrence([{a: 1, b: 2}, {a:1, b:3}, {a:1, b:2}]) // {a:1, b:2}
+evenOccurrence([{a: 1, b: 2}, {a:1, b:3}, {a:1, b:2}])  // {a:1, b:2}
