f0e14e894c478e891c4adf0f8dc7eb1d20f54ac8
Sofian-Saleh <56444632+SofianSaleh@users.noreply.github.com>
2020-01-17 10:22:09 +0200

    Add files via upload

---
+++ InsertionSort/Insertionsort .js
@@ -0,0 +1,45 @@
+// Sort a linked list using insertion sort.
+// Algorithm of Insertion Sort:
+
+//1- Insertion sort iterates, consuming one input element each repetition, and growing a sorted output list.
+//2- At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it there.
+// 3-It repeats until no input elements remain.
+
+// Example 1:
+
+// Input: 4->2->1->3
+// Output: 1->2->3->4
+// Example 2:
+
+// Input: -1->5->3->4->0
+// Output: -1->0->3->4->5
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function(head) {
+  debugger;
+  for (let i = 0; i < head.length; i++) {
+    var element = head[i];
+    var j;
+    console.log(head, element);
+    for (j = i - 1; j >= 0 && head[j] > element; j--) {
+      // if (head[j] > element) {
+      head[j + 1] = head[j];
+      // }
+    }
+    head[j + 1] = element;
+  }
+
+  return head;
+};
+
+insertionSortList([4, 2, 1, 3]);
