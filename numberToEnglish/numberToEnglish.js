d757d60d20249972250bd2cd910495f01aeb7e19
Sofian-Saleh <56444632+SofianSaleh@users.noreply.github.com>
2020-01-21 11:26:43 +0200

    doneish

---
+++ numberToEnglish/numberToEnglish.js
@@ -0,0 +1,96 @@
+/*
+Number to English
+
+Write a function numberToEnglish, it should take a number and return the number as a string using English words.
+
+
+numbertoEnglish(7) // "seven"
+numberToEnglish(575) // "five hundred seventy-five"
+numberToEnglish(78193512) // "seventy-eight million one hundred ninety-three thousand five hundred twelve"
+*/
+
+// HELPERS
+
+var numbersToWords = {
+  0: "zero",
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  11: "eleven",
+  12: "twelve",
+  13: "thirteen",
+  14: "fourteen",
+  15: "fifteen",
+  16: "sixteen",
+  17: "seventeen",
+  18: "eighteen",
+  19: "nineteen",
+  20: "twenty",
+  30: "thirty",
+  40: "forty",
+  50: "fifty",
+  60: "sixty",
+  70: "seventy",
+  80: "eighty",
+  90: "ninety"
+};
+
+var numbersToPlace = {
+  10: "ten",
+  100: "hundred",
+  1000: "thousand",
+  1000000: "million",
+  1000000000: "billion",
+  1000000000000: "trillion",
+  1000000000000000: "quadrillion",
+  1000000000000000000: "quintillion"
+};
+
+function numberToEnglish(number) {
+  var origin;
+  var currentNumber;
+  var result = "";
+  var lenLeft;
+  var place;
+
+  if (number < 20) {
+    return numbersToWords[number];
+  } else if (number < 100) {
+    currentNumber = Math.floor(number / 10) * 10;
+    lenLeft = number % 10;
+    if (lenLeft === 0) {
+      result = `${numbersToWords[currentNumber]}`;
+    } else {
+      result = `${numbersToWords[currentNumber]}-${numbersToWords[lenLeft]}`;
+    }
+    return result;
+  } else {
+    if (number < 1000) {
+      place = 100;
+    } else {
+      place = 1000;
+      while (place * 100 <= number) {
+        place *= 1000;
+      }
+    }
+    currentNumber = Math.floor(number / place);
+    lenLeft = number % place;
+    result = numberToEnglish(currentNumber) + " " + numbersToPlace[place];
+    var rest;
+    rest = numberToEnglish(lenLeft);
+    if (rest === "zero") {
+      return result;
+    }
+    return (result = result + " " + rest);
+  }
+
+  return result;
+}
+console.log(numberToEnglish(575));
