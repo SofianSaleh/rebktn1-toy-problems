39fccfd5860e28698c6a892afaaed6d4138b485e
Sofian-Saleh <56444632+SofianSaleh@users.noreply.github.com>
2020-02-12 09:43:47 +0100

    done

---
+++ robotPaths/robotPaths.js
@@ -0,0 +1,80 @@
+/*
+Robot Paths
+
+A robot located at the top left corner of an n x n grid is trying to reach the bottom right corner. 
+The robot can move either up, down, left, or right, but cannot visit the same spot twice. 
+How many possible unique paths are there to the bottom right corner?
+
+Make your solution work for a grid of any size.
+
+Parameters:
+
+n (required) - amount of rows/columns (max 6)
+
+Example: 
+
+robotPaths(1) // 1
+robotPaths(2) // 2
+robotPaths(3) // 12
+
+*/
+
+// Helpers
+
+function makeBoard(n) {
+  var board = [];
+  for (var i = 0; i < n; i++) {
+    board.push([]);
+    for (var j = 0; j < n; j++) {
+      board[i].push(false);
+    }
+  }
+  board.togglePiece = function (i, j) {
+    this[i][j] = !this[i][j];
+  };
+  board.hasBeenVisited = function (i, j) {
+    console.log(this[i][j])
+    return !!this[i][j];
+  };
+  return board;
+}
+
+
+function robotPaths(n) {
+  var board = makeBoard(n);
+  var ways = 0;
+
+  var recurse = (i, j) => {
+    board.togglePiece(i, j);
+    if (i === n - 1 && j == n - 1) {
+      ways++;
+      return;
+    }
+    // down
+    if (i + 1 < n && !board[i + 1][j]) {
+      recurse(i + 1, j);
+      board.togglePiece(i + 1, j);
+    }
+    // right
+    if (j + 1 < n && !board[i][j + 1]) {
+      recurse(i, j + 1);
+      board.togglePiece(i, j + 1);
+    }
+    //up
+    if (i - 1 >= 0 && !board[i - 1][j]) {
+      recurse(i - 1, j);
+      board.togglePiece(i - 1, j);
+    }
+    //left
+    if (j - 1 >= 0 && !board[i][j - 1]) {
+      recurse(i, j - 1);
+      board.togglePiece(i, j - 1);
+    }
+
+  };
+
+  recurse(0, 0);
+  return ways;
+}
+
+console.log(robotPaths(5))
